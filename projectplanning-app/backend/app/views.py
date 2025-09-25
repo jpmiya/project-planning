a03cc5fdc5990e2c1e9a670a9483e232a2e98ea7
juampi <jpmiya82@gmail.com>
2025-09-25 12:17:52 -0300

    changes

---
+++ projectplanning-app/backend/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib import messages
+
+# Create your views here.
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+        
